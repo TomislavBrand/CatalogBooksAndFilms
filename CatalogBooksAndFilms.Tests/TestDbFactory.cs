a213cf8571efc3e17ea7415ae3b384a13770c1f3
Krisss <atomhd445@gmail.com>
2026-02-16 19:25:38 +0200

    Added tests

---
+++ CatalogBooksAndFilms.Tests/TestDbFactory.cs
@@ -0,0 +1,22 @@
+﻿using CatalogBooksAndFilms.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogBooksAndFilms.Tests
+{
+    public static class TestDbFactory
+    {
+        public static ApplicationDbContext CreateDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            return new ApplicationDbContext(options);
+        }
+    }
+}
--- CatalogBooksAndFilms.Tests/UnitTest1.cs
+++ CatalogBooksAndFilms.Tests/UnitTest1.cs
@@ -1,11 +1,31 @@
+using CatalogBooksAndFilms.Entities;
+using CatalogBooksAndFilms.Services.Implementations;
+using Xunit;
+
 namespace CatalogBooksAndFilms.Tests
 {
-    public class UnitTest1
+    public class AuthorServiceTests
     {
         [Fact]
-        public void Test1()
+        public async Task GetAllAsync_ReturnsAllAuthors()
         {
+            // Arrange
+            using var db = TestDbFactory.CreateDbContext();
 
+            db.Authors.AddRange(
+                new Author { Name = "Author1" },
+                new Author { Name = "Author2" }
+            );
+
+            await db.SaveChangesAsync();
+
+            var service = new AuthorService(db);
+
+            // Act
+            var result = await service.GetAllAsync();
+
+            // Assert
+            Assert.Equal(2, result.Count);
         }
     }
 }
